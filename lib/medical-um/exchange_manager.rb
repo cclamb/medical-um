cf4bb43476abe8eefc87c5579d2dec7064dcd6ab
chris <cclamb@cs.unm.edu>
2010-11-02 16:10:37 -0600

    added exmgr methods

--- lib/medical-um/exchange_manager.rb
+++ lib/medical-um/exchange_manager.rb
@@ -1,3 +1,26 @@
 class ExchangeManager
   
+  def register_user(user_info)
+  end
+  
+  def remove_user(user_info)
+  end
+  
+  def get_user(id) 
+  end
+  
+  def update_user(user_info)
+  end
+  
+  def initiate_negotiation
+  end
+  
+  def update_negotiation(negotiation_info)
+  end
+  
+  def get_negotiation(id)
+  end
+  
+  
+  
 end
