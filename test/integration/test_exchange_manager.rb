601fcaf6870144f99a14e53211badc45961b56fd
chris <cclamb@cs.unm.edu>
2010-11-04 10:08:54 -0600

    updating test reorg

---
+++ test/integration/test_exchange_manager.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../test_helper.rb'
+require 'medical-um/basic_user_repository'
+require 'test/unit'
+
+class TestExchangeManager < Test::Unit::TestCase
+
+  def setup
+  end
+  
+  def test_truth
+    assert true
+  end
+end
--- test/unit/test_basic_user_repository.rb
+++ test/unit/test_basic_user_repository.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/test_helper.rb'
+require File.dirname(__FILE__) + '/../test_helper.rb'
 require 'medical-um/basic_user_repository'
 require 'test/unit'
 
